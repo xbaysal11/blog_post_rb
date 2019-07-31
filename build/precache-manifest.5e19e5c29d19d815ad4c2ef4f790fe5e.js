78978ff24e52d6bf24aef092522bc9bcb867d060
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-07-31 14:36:06 +0600

    :seedling: ADDED build

---
+++ build/precache-manifest.5e19e5c29d19d815ad4c2ef4f790fe5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "409891a656f65083ac766b52b9977e47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e82084e33c4189b031e4",
+    "url": "/static/css/main.13a5908a.chunk.css"
+  },
+  {
+    "revision": "b2d5c418d860c1f68055",
+    "url": "/static/js/2.d675d96f.chunk.js"
+  },
+  {
+    "revision": "e82084e33c4189b031e4",
+    "url": "/static/js/main.a4c2de92.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
